c0532513692d4f3a3ab1f65d45bbee6bbef76dfc
krisvers <nakoidevn@gmail.com>
2022-10-02 12:35:23 -0400

    Updated main.c

--- main.c
+++ main.c
@@ -1,3 +1,39 @@
+/*
+ 	Copyright (c) 2022 Nicholas Kelly
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+
+	catsay:
+	 _______________________________________________________________
+	(This is catsay! A terrible recreation of cowsay but with a cat.)
+	(_____________________________________________________________ _)
+	                                                              v
+	                                                             ^__^
+	                                                            (,  ,)
+	                                                            >/7Y/<
+	                                                            (    )
+	                                                            | __ |
+	                                                            | || |
+	                                                            [_][_]
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 
@@ -30,7 +66,7 @@ int main(int argc, char ** argv)
 
 	unsigned char vpos = 0;
 
-	printf("\n ");
+	printf(" ");
 	for (unsigned char i = 1; i <= strlen(argv[1]); i++)
 	{
 		printf("_");
